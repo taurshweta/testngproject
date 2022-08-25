4a1b1bf410fc0cf3e4544c2f87bfe5733fbb9481
User <User@DESKTOP-VVD3BPA>
2022-08-25 20:57:30 +0530

    checking invocation count changes

--- TestNGpractice/src/Testngprograms/Checkinvokationcount.java
+++ TestNGpractice/src/Testngprograms/Checkinvokationcount.java
@@ -3,11 +3,11 @@ package Testngprograms;
 import org.testng.annotations.Test;
 
 public class Checkinvokationcount {
-	@Test(invocationCount = 7)
+	@Test(invocationCount = 10)
 	public void write() {
 		System.out.println("write method");
 	}
-	@Test(invocationCount=5)
+	@Test(invocationCount= 7)
 	public void read()
 	{
 		System.out.println("read method");
